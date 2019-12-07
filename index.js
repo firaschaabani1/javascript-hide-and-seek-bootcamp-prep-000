05a97186672f4061e82551de5d260832ec9f334e
Firas Chaabani <firaschaabani1@gmail.com>
2019-12-07 01:29:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+function getFirstSelector('selector') {
+while {
+  var ('element') = 'selector'
+  return element ()
+}
+  else {
+    return null
+  }
+}
+function nestedTarget () {nested; target}
+var selector = '#nested'
